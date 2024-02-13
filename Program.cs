5b0eb2095524f78155a3916460206e89363046d9
juliaaa2 <142424951+juliaaa2@users.noreply.github.com>
2024-02-13 11:29:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace appConsDemo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double varLeftOp = clsUtilities.opReadNumberWith<int>("Left operand");
+            double varRightOp = clsUtilities.opReadNumberWith<int>("Right operand");
+            Console.Write(clsUtilities.opAdd(varLeftOp, varRightOp));
+            Console.ReadKey();
+        }
+    }
+}
